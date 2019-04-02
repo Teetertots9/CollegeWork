1a3a85f0984afb44ff3766aaf435dcc6a5ea1e42
X00139120 <X00139120@computing.stu.it-tallaght.ie>
2019-04-02 15:34:31 +0100

    lab 3 sample ca started, on test

---
+++ SDEV4Labs/src/Lab3SampleCA/Concert.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lab3SampleCA;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author x00139120
+ */
+public class Concert {
+    private String performer;
+    private int concertCapacity;
+    private List<Ticket> tickets = new ArrayList<>();
+    
+    public Concert(String performer, int concertCapacity){
+        this.performer = performer;
+        this.concertCapacity = concertCapacity;
+    }
+    
+    public void fillList(Ticket ticket){
+        tickets.add(ticket);
+    }
+    
+    public String getPerformer(){
+        return performer;
+    }
+    
+    public void setPerformer(String performer){
+        this.performer = performer;
+    }
+    
+    public int getCapacity(){
+        return concertCapacity;
+    }
+    
+    public void setCapacity(int concertCapacity){
+        this.concertCapacity = concertCapacity;
+    }
+    
+    public List<Ticket> getTickets(){
+        return tickets;
+    }
+    
+    public void setTickets(List<Ticket> tickets){
+        this.tickets = tickets;
+    }
+    
+    public void display(){
+        System.out.println("Concert performed by: " + performer);
+        for (Ticket ticket : tickets) {
+            System.out.println(ticket.getTicketType().getPrice() + ", " + ticket.getTicketType());
+        }
+        System.out.println("Total sales: " + totalSales());
+    }
+    
+    public double totalSales(){
+        double sales = 0;
+        for (Ticket ticket : tickets) {
+            sales += ticket.getTicketType().getPrice();
+        }
+        return sales;
+    }
+    class Ticket {
+    private ConcertTicket ticketType;
+    
+    public Ticket(ConcertTicket ticketType){
+        this.ticketType = ticketType;
+    }
+    
+    public ConcertTicket getTicketType(){
+        return ticketType;
+    }
+}
+}
+
